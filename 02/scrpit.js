e77a19a92be84b88cb587f96073255d38fa59d89
Michel <michelhazy@gmail.com>
2024-01-30 13:35:01 -0300

    dois min

---
+++ 02/scrpit.js
@@ -0,0 +1,12 @@
+
+let todosNumeros = [];
+
+function numeroPrimo(num){
+    if(num % 2 == 0){
+        console.log(num + " nao e primo")
+    } else {
+        console.log(num + " e primo")
+    }
+}
+
+numeroPrimo(842342);
